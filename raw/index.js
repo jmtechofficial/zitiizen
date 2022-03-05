5532db57af6ce4ecc9019c21aa7a4397db732263
jmresult <89685675+jmresult@users.noreply.github.com>
2022-03-05 17:16:42 +0100

    initial push

--- raw/index.js
+++ raw/index.js
@@ -5,3 +5,11 @@ let _BODY = decodeURIComponent(atob("JTBBJTNDZGl2JTIwY2xhc3MlM0QlMjJjaXRpemVucy1
 let _HTML_CLASSES = "js flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths citizens-Firefox citizens-user-none";
 
 let _BODY_CLASS = "responsive-enabled";
+
+
+
+document.querySelector("html").classList.add(_HTML_CLASSES);
+document.body.classList.add(_BODY_CLASS);
+document.head.append(_HEAD);
+document.body.prepend(_BODY);
+
