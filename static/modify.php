77a5eea4168334af1c0f98806b0036d67ad5ae11
jmresult <89685675+jmresult@users.noreply.github.com>
2022-03-06 23:47:31 +0100

    initial push

--- static/modify.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE html>
-<html class="js flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths citizens-Firefox citizens-user-none" lang="en-US">
-<head>
-</head>
-<body class="responsive-enabled">
-
-</body>
-</html>
